6b21f895549ed52872b9d7c438b7daa664602959
Hieu Nguyen <hieutrung.nguyen@isobar.com>
2021-10-24 00:20:49 +0700

    deploy

---
+++ gh-pages.js
@@ -0,0 +1,17 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+	'public', // path to public directory
+	{
+		branch: 'main',
+		repo: 'https://github.com/vanhieu95/sevlte-feedback-app.git', // Update to point to your repository
+		user: {
+			name: 'Hieu Nguyen', // update to use your name
+			email: 'nguyenluongtrunghieu1995@gmail.com' // Update to use your email
+		},
+		dotfiles: true
+	},
+	() => {
+		console.log('Deploy Complete!');
+	}
+);
